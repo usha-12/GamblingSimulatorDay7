5f49bfba48698152b005a4f90ef1f0b0cd99f7a6
usha-12 <usha12ahirwar@gmail.com>
2022-09-09 11:44:33 +0530

    This problem simulates a Gambler who starts with a stake and bets every game to win or loose some stake. Being a Calculative Gambler exits if the Stake reaches a high or a low limit

---
+++ src/Gambling/GamblingSimulator.java
@@ -0,0 +1,9 @@
+package Gambling;
+
+public class GamblingSimulator {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the gambling Simulator ");
+        System.out.println("This problem simulates a Gambler who starts with a stake and bets every game to win or loose some stake. Being a Calculative Gambler exits if the Stake reaches a high or a low limit");
+        GamblingSimulator gambler = new GamblingSimulator();
+    }
+}
