13744a59ecc236360445fdbcf3b32d807217ea7a
usha-12 <usha12ahirwar@gmail.com>
2022-09-09 11:51:58 +0530

    As a Gambler, would start with a stake of $100 every day and bet $1 every game.

--- src/Gambling/GamblingSimulator.java
+++ src/Gambling/GamblingSimulator.java
@@ -1,9 +1,20 @@
 package Gambling;
 
 public class GamblingSimulator {
+    public int funds =0;
+    public int bet = 0;
     public static void main(String[] args) {
         System.out.println("Welcome to the gambling Simulator ");
         System.out.println("This problem simulates a Gambler who starts with a stake and bets every game to win or loose some stake. Being a Calculative Gambler exits if the Stake reaches a high or a low limit");
         GamblingSimulator gambler = new GamblingSimulator();
+        gambler.gameInitialise();
+
     }
+    public void gameInitialise(){
+        System.out.println("Initialising game.....");
+        funds=100;
+        bet=1;
+        System.out.println("Funds: "+funds);
+    }
+    /*As a Gambler, would start with a stake of $100 every day and bet $1 every game.*/
 }
